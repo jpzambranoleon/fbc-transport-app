70a1956daeb799ac5ed107f7a878e572506c4d00
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-30 22:18:08 -0500

    imported axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import { Box } from "@mui/material";
+import axios from "axios";
+import { useContext } from "react";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -5,15 +8,22 @@ import {
   Routes,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import { InfoContext } from "./utility/InfoProvider";
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:4400/api";
+
+  const { authorized } = useContext(InfoContext);
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Box bgcolor={"background.default"} color="text.primary">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </Box>
     </div>
   );
 }
