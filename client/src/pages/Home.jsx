bb5b5a4fe1d880d963775a95191788165b380536
Jean-Paul <jpzambrano.leon@gmail.com>
2022-11-01 12:26:23 -0500

    modified Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,56 +1,16 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { InfoContext } from "../utility/InfoProvider";
+import Feed from "../components/Feed";
 
 export default function Home() {
-  // const { authorized } = useContext(InfoContext);
+  const { authorized } = useContext(InfoContext);
 
   return (
     <main>
-      <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="primary"
-            gutterBottom
-          >
-            Team Finder
-          </Typography>
-          <Typography
-            variant="h5"
-            align="center"
-            color="text.secondary"
-            paragraph
-          >
-            Looking for some people to play football with? Are you looking for a
-            team to join? We can help you with that. Find a team near you!
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() =>
-                window.scrollTo({
-                  top: document.getElementById("filter").offsetTop,
-                  behavior: "smooth",
-                })
-              }
-            >
-              Find a Post
-            </Button>
-            <Button component={Link} variant="outlined" color="primary">
-              Create a Post
-            </Button>
-          </Stack>
-        </Container>
+      <Box sx={{ mt: 2 }}>
+        <Feed />
       </Box>
     </main>
   );
