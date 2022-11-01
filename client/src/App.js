0b20427575f8b482b79c1ec1d73fa810c5439435
Jean-Paul <jpzambrano.leon@gmail.com>
2022-10-31 22:21:57 -0500

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import axios from "axios";
 import { useContext } from "react";
 import {
@@ -7,18 +7,23 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { InfoContext } from "./utility/InfoProvider";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:4400/api";
 
-  const { authorized } = useContext(InfoContext);
+  // const { authorized } = useContext(InfoContext);
 
   return (
     <div className="App">
       <Box bgcolor={"background.default"} color="text.primary">
         <Router>
+          <Navbar />
+          <Grid container>
+            <Grid item sm={3}></Grid>
+          </Grid>
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
