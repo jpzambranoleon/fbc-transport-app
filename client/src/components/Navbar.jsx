3c875e4fe770eca4037ed1632139bae54ed5c15d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-11-01 00:58:16 -0500

    modified Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,41 @@
 import {
   AppBar,
+  Avatar,
   Box,
   Button,
   IconButton,
+  Skeleton,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useContext } from "react";
+import { InfoContext } from "../utility/InfoProvider";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Navbar = () => {
+  const { authorized } = useContext(InfoContext);
+  const userId = localStorage.getItem("user");
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES_PERSON;
+
+  useEffect(() => {
+    if (authorized) {
+      setLoading(true);
+      const fetchUser = async () => {
+        const res = await axios.get(`/users?userId=${userId}`);
+        setUser(res.data);
+        setLoading(false);
+      };
+      fetchUser();
+    }
+  }, [authorized, userId]);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -26,6 +53,18 @@ const Navbar = () => {
             News
           </Typography>
           <Button color="inherit">Login</Button>
+          <IconButton>
+            {loading ? (
+              <Skeleton variant="circular" width={35} height={35} />
+            ) : (
+              <Avatar
+                sx={{ height: 35, width: 35 }}
+                src={
+                  !user.profilePic ? "/broken-image.jpg" : PF + user.profilePic
+                }
+              />
+            )}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
