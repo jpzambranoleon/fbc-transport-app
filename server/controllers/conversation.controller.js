ef3a59ae893086901409e71d03c49805a55a970c
Jean-Paul <jpzambrano.leon@gmail.com>
2022-11-01 23:37:39 -0500

    created conversation.controller.js

---
+++ server/controllers/conversation.controller.js
@@ -0,0 +1,34 @@
+const Conversation = require("../models/conversation.model");
+const router = require("../routes/auth.routes");
+
+// New conversation
+exports.newConversation = async (req, res) => {
+  let newConvo = new Conversation({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+
+  try {
+    let savedConversation = await newConvo.save();
+    res.status(200).json(savedConversation);
+  } catch (err) {
+    res.status(500).json({
+      error: true,
+      message: err.message,
+    });
+  }
+};
+
+// Get conversation of a user
+exports.getConversation = async (req, res) => {
+  try {
+    let conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json({
+      error: true,
+      message: err.message,
+    });
+  }
+};
