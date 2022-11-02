bcc67a024de2035419b91a64ab198f173a57661f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-11-01 23:38:27 -0500

    created conversation.model.js

---
+++ server/models/message.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema(
+  {
+    conversationId: {
+      type: String,
+    },
+    sender: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("message", messageSchema);
+module.exports = Message;
