7b6cce654af5998e7e265cb30a0a3f0914e6ff29
Jean-Paul <jpzambrano.leon@gmail.com>
2022-11-30 15:58:16 -0600

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,11 @@ import {
 } from "react-router-dom";
 import Leftbar from "./components/Leftbar";
 import Navbar from "./components/Navbar";
+import ChatRoom from "./pages/ChatRoom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Messagees from "./pages/Messages";
+import Messages from "./pages/Messages";
+import Messages2 from "./pages/Messages2";
 import Profile from "./pages/Profile";
 import ProfileEdit from "./pages/ProfileEdit";
 import Register from "./pages/Register";
@@ -32,7 +34,8 @@ function App() {
             <Container>
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/messages" element={<Messagees />} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/chatroom" element={<ChatRoom />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/profile/edit" element={<ProfileEdit />} />
               </Routes>
