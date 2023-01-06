640eba318a0a0b3ed960e45cf278cce567228da8
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-06 17:28:58 -0600

    created Conversation.jsx

---
+++ client/src/components/Conversation.jsx
@@ -0,0 +1,15 @@
+import { Avatar, Box, Typography } from "@mui/material";
+
+const Conversation = () => {
+  return (
+    <Box>
+      <Avatar
+        alt="Remy Sharp"
+        src="https://material-ui.com/static/images/avatar/1.jpg"
+      />
+      <Typography>Remy Sharp</Typography>
+    </Box>
+  );
+};
+
+export default Conversation;
