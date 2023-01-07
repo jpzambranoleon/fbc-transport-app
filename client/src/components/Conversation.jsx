d3f48e71d964fd8ec69c78fd14a840a369dceb9c
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-06 20:44:56 -0600

    updated Conversatio.jsx

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -1,14 +1,43 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
-const Conversation = () => {
+const Conversation = ({ conversation, currentUser }) => {
+  const [user, setUser] = useState({});
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES_PERSON;
+
+  useEffect(() => {
+    const friendId = conversation.members.find((m) => m !== currentUser._id);
+
+    const getUser = async () => {
+      try {
+        const res = await axios.get(`/users?userId=${friendId}`);
+        setUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUser();
+  }, [currentUser, conversation]);
+
   return (
-    <Box>
-      <Avatar
-        alt="Remy Sharp"
-        src="https://material-ui.com/static/images/avatar/1.jpg"
-      />
-      <Typography>Remy Sharp</Typography>
-    </Box>
+    <ListItemButton>
+      <ListItemIcon>
+        <Avatar
+          alt={user.username}
+          src={!user.profilePic ? "/broken-image.jpg" : PF + user.profilePic}
+        />
+      </ListItemIcon>
+      <Typography>{user.username}</Typography>
+    </ListItemButton>
   );
 };
 
